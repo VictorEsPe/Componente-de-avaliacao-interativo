ab44431c2c0c611ee4f3504325e5a91942e956d0
Victor Estevam Pereira <victorestevamp@gmail.com>
2023-03-05 11:04:43 -0300

    Limpeza do código e correção de bug

--- js/script.js
+++ js/script.js
@@ -2,23 +2,19 @@ const ratingContainer = document.querySelector(".rating-container")
 const thankYouContainer = document.querySelector(".thank-you-container")
 const ratings = document.querySelectorAll(".btn")
 const chosenRating = document.getElementById("chosen-rating")
-const submitButton = document.getElementById("submit-btn")
-const closeButton = document.getElementById("close-btn")
+
+ratings.forEach((rating) => {
+  rating.addEventListener("click", () => {
+    chosenRating.innerHTML = rating.innerHTML
+  })
+})
 
 function showThankYouCard() {
   ratingContainer.classList.add("hidden")
   thankYouContainer.classList.add("show")
-
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      chosenRating.innerHTML = rating.innerHTML
-    })
-  })
 }
 
 function closeThankYouCard() {
   ratingContainer.classList.remove("hidden")
   thankYouContainer.classList.remove("show")
 }
-
-// TODO: Corrigir um bug que faz com que a nota escolhida não apareça no card de agradecimento de primeira
